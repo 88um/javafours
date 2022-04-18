89164135ecead541dcb99db4b4c209a016f7f480
Joshua Solo <97570113+88um@users.noreply.github.com>
2022-04-18 15:04:41 -0500

    Update and rename Try.java to Generate.java

--- Generate.java
+++ Generate.java
@@ -1,8 +1,8 @@
 import java.util.*;
 import java.io.*;  
-import java.io.IOException;  
+ 
 
-public class Try{ 
+public class Generate{ 
     public static char[] items = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     public static Random rand = new Random();
     public static Scanner sc = new Scanner(System.in);
